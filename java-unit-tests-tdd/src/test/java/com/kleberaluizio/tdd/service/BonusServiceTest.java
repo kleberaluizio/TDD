a039a4e55a46cb80629dadb141bf9e6d7eeb8e12
kleberaluizio <kleberaluizio@gmail.com>
2023-07-03 08:57:43 -0300

    Adding tests for BonusService class

---
+++ java-unit-tests-tdd/src/test/java/com/kleberaluizio/tdd/service/BonusServiceTest.java
@@ -0,0 +1,39 @@
+package com.kleberaluizio.tdd.service;
+
+import com.kleberaluizio.tdd.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void bonusShouldBe10PercentOfSalary(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calculateBonus(
+                new Employee("Joe", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.0"),bonus);
+    }
+    @Test
+    void bonusShouldBe10PercentOfSalaryIfSalaryEquals1000(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calculateBonus(
+                new Employee("Joe", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.0"),bonus);
+    }
+
+    @Test
+    void bonusShouldBeZeroToHighPaidEmployee(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calculateBonus(
+                new Employee("Joe", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+}
